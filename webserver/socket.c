df7f50e509a1dcf6ce612ad4799262d6ea7cd110
Louis Guilbert <louis.guilbert@etudiant.univ-lille1.fr>
2015-01-19 15:23:58 +0100

    ajout constantes

--- webserver/socket.c
+++ webserver/socket.c
@@ -5,7 +5,7 @@
 #include "socket.h"
 
 
-int creer_serveur (int port )
+int creer_serveur (int port)
 {
 	int socket_server = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_server == -1)
@@ -16,7 +16,7 @@ int creer_serveur (int port )
 	
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(8000);
+	saddr.sin_port = htons(port);
 	saddr.sin_addr.s_addr = INADDR_ANY;
 	
 	if (bind(socket_server, (struct sockaddr *) &saddr, sizeof(saddr)) == -1)
