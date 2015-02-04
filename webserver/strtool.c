71703476afb7b8e65ee55029b4da8e98388a354e
Nicolas Crosetti <nicolas.crosetti@etudiant.univ-lille1.fr>
2015-02-04 09:33:12 +0100

    fonction is_valid_line

--- webserver/strtool.c
+++ webserver/strtool.c
@@ -7,3 +7,13 @@ int parse_request(char* request, char* method, char* url, int* version_M, int* v
 {
 	return sscanf(request, "%s %s HTTP/%d.%d", method, url, version_M, version_m);
 }
+
+/* Retourne le nombre de caractères lus sur la ligne 
+   ou -1 si la ligne ne se termine pas par \r\n */
+int is_valid_line(char* line) {
+  char buf[255];
+  if (sscanf(line, "%s\r\n", buf) != 1) {
+    return -1;
+  }
+  return strlen(buf);
+}
