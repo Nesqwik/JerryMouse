456db13957b5fdd27ee8fe44f0da9a6f2e08c33b
Louis Guilbert <louis.guilbert@etudiant.univ-lille1.fr>
2015-03-11 09:48:15 +0100

    ajout du sémaphore

--- webserver/stats.c
+++ webserver/stats.c
@@ -1,12 +1,16 @@
 #include <sys/mman.h>
 #include <stdio.h>
+#include <semaphore.h>
+
 #include "stats.h"
 
 static web_stats* stats;
+static sem_t* sem;
 
 int init_stats(void) 
 {
 	stats = mmap(NULL, sizeof(web_stats), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
 	stats->served_connections = 0;
 	stats->served_requests = 0;
@@ -15,6 +19,7 @@ int init_stats(void)
 	stats->ko_403 = 0;
 	stats->ko_404 = 0;
 	
+	sem_init(sem, 42, 1);
 	return 0;
 }
 
@@ -26,6 +31,7 @@ web_stats *get_stats(void)
 
 void increment_stats(int code)
 {
+	sem_wait(sem);
 	web_stats* stats = get_stats();
 	switch(code)
 	{
@@ -52,4 +58,6 @@ void increment_stats(int code)
 			stats->served_requests += 1;
 			break;
 	}
+
+	sem_post(sem);
 }
